d4df37c0b6ed709d73e0586cb0eb8726f0320d26
Sella N P <sella13300@gmail.com>
2016-11-09 17:01:34 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview1/adapter/HotelAdapter.java
@@ -0,0 +1,7 @@
+package id.sch.smktelkom_mlg.learn.recyclerview1.adapter;
+
+/**
+ * Created by Smktelkom on 09-Nov-16.
+ */
+public class HotelAdapter {
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview1/hotel/Hotel.java
@@ -0,0 +1,7 @@
+package id.sch.smktelkom_mlg.learn.recyclerview1.hotel;
+
+/**
+ * Created by Smktelkom on 09-Nov-16.
+ */
+public class Hotel {
+}
